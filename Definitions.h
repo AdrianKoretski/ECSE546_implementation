4b12c384f19872eecafce2ef63004f0fed254bcf
AdrianKoretski <34486058+AdrianKoretski@users.noreply.github.com>
2019-11-19 08:01:26 -0500

    Created some classes and structs

--- Definitions.h
+++ Definitions.h
@@ -5,3 +5,14 @@
 #define v4f glm::vec4
 #define v3f glm::vec3
 #define v2f glm::vec2
+
+class Texture2D;
+
+struct vertex
+{
+	v4f position;
+	v3f color;
+	v3f normal;
+	v2f texture;
+	Texture2D* texture;
+};
